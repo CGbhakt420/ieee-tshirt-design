f5bd3e67504d9e59184e6e58b6956fee77121345
Sanchit Kumar <sanchit.kr007@gmail.com>
2025-07-22 23:46:52 +0530

    add deployed backend url for api target

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://threed-tshirt-design-kv3l.onrender.com",
         changeOrigin: true,
         secure: false,
       },
