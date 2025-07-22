62734a974b80e2e3cad4fea84fc8c5371984456e
Sanchit Kumar <sanchit.kr007@gmail.com>
2025-07-23 00:17:10 +0530

    route fix

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,9 @@ mongoose.connect(process.env.MONGO_URI,{
 app.get('/',protect,(req,res)=>res.send('API running'));
 
 // TODO:ADD Routes
-app.use('/api/auth', require('./routes/auth.js'));
-app.use('/api/designs', require('./routes/designs.js'));
-app.use('/api/community',require('./routes/posts.js'));
+app.use('/auth', require('./routes/auth.js'));
+app.use('/designs', require('./routes/designs.js'));
+app.use('/community',require('./routes/posts.js'));
 
 const PORT =process.env.PORT || 5000;
 
